3262df0c87433073bfdf07d00ac95fdd9c9e8c21
EC2 Default User <ec2-user@ip-172-31-10-86.ec2.internal>
2022-03-13 03:04:21 +0000

    commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+   def new
+    @list = List.new
+   end
+
+   def create
+
+    list = List.new(list_params)
+    list.save
+    # トップへ遷移→showに
+    redirect_to'/'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'homes/top' => 'homes#top'
+  # 規定のURlに変更予定
+  get '/' => 'homes#top'
+  get 'lists/new' =>'lists#new'
+  post 'lists' => 'lists#create'
+  get 'lists/index' => 'lists#index'
+  get 'lists/show' =>'lists#show'
+  get 'lists/edit' => 'lists#edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
