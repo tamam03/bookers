54260f9de417651ed7c4365a7c99fef1a807a8ec
EC2 Default User <ec2-user@ip-172-31-10-86.ec2.internal>
2022-03-14 07:41:27 +0000

    index_html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     if@book.save
       redirect_to book_path(@book.id),notice:'Book was successfully created.'
     else
-      render :new
+      render :index
     end
   end
 
