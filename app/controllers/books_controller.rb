8bfb70e5546e3759df1bcc8d61407dec51905149
EC2 Default User <ec2-user@ip-172-31-10-86.ec2.internal>
2022-03-14 05:23:52 +0000

    routes commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,51 @@
 class BooksController < ApplicationController
-  def top
+
+
+  def create
+     @book = Book.new(book_params)
+    if@book.save
+      redirect_to book_path(@book.id),notice:'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
+   def index
+     @books = Book.all
+     @book =Book.new
+   end
+
+   def show
+      @book = Book.find(params[:id])
+   end
+
+   def edit
+      @book = Book.find(params[:id])
+   end
+
+   def update
+     @book = Book.find(params[:id])
+     if@book.update(book_params)
+      redirect_to book_path(@book.id),notice:'Book was successfully updated.'
+     else
+      render :edit
+     end
+   end
+
+   def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to '/books',notice:'Book was successfully destroyed.'
+
+   end
+
+
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  root :to => 'books#top'
-  get 'books/new' =>'lists#new'
-  post 'books' => 'lists#create'
-  get 'books' => 'lists#index'
-  get 'books/:id' => 'lists#show',as:'list'
-  get 'books/:id/edit' => 'lists#edit',as:'edit_list'
-  patch 'books/:id' => 'lists#update',as:'update_list'
-  delete 'books/:id' => 'lists#destroy',as:'destroy_list'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'homes#top'
+  resources :books
+  
 end
